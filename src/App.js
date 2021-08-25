ed29e3b39d630d9810197a6cd7ce13a73236a562
dmytroBohomaz <bogomaz75@i.ua>
2021-08-25 12:09:23 +0300

    release v1

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DMYTRO <code>BOHOMAZ</code> fixed it.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        >G
+          Learn React and Git and css
         </a>
       </header>
     </div>
